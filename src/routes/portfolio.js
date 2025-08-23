a9f75268450e7a9c86ac3bbb7fd4b35d720dc8c0
dilip27m <mdilip2909@gmail.com>
2025-08-23 17:06:59 +0530

    feat:implemented feature To save,edit and the fetch the data

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const {
+  savePortfolio,
+  getPortfolio,
+} = require("../controllers/portfolioController");
 
-router.get("/", (req, res) => {
-  res.send("Portfolio route working 🚀");
-});
+// This route will handle saving and updating the portfolio data.
+// It points to POST http://localhost:5000/api/portfolio
+router.post("/", savePortfolio);
+
+// This route will handle fetching a portfolio's data by their ID.
+// It points to GET http://localhost:5000/api/portfolio/someUserId
+router.get("/:userId", getPortfolio);
 
 module.exports = router;
